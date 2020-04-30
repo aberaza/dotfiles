9e286e10bc93ad04c5a2bdc038b3f7db980b5ed5
Aritz Beraza <aritz.beraza@gmail.com>
2020-04-30 10:13:34 +0200

    True color support for tmux; Esc delay in vim removed

--- bin/toPdf.sh
+++ bin/toPdf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandoc $1 -o $1.pdf --from markdown --template eisvogel --number-sections -F mermaid-filter -V lang=es-ES --pdf-engine xelatex
+pandoc $1 -o $1.pdf --from markdown --template eisvogel --number-sections -F mermaid-filter -V lang=es-ES --latex-engine xelatex
